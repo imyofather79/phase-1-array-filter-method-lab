0822e935e715fc4ef0886ff93a0aab6ff24ad0da
imyofather79 <chunsum708@gmail.com>
2021-05-30 17:12:43 -0600

    Missing one

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    
+    return drivers.filter (driver => driver.toLowerCase() == name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    const newDrivers = drivers.filter(function (initial){
+        return initial.slice(0,1) === name.slice(0,1);
+    });
+    return newDrivers;
+}
+
+function matchName(collection){
+    // const matchDriver = drivers.filter(function filter (collection){
+    // for (const drivers of collection){
+    //     if (drivers.filter(driver => driver.toLowerCase() == name.toLowerCase())){
+    //        console.log(driver);
+    //         // return drivers;
+
+    //     }
+    // }
+    }
